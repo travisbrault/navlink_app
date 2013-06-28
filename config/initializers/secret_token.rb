7ffcd73bc304e02a50c924110ed2f4607bbf60b9
travisbrault <brault.travis@gmail.com>
2013-06-28 15:00:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NavlinkApp::Application.config.secret_token = 'b6561c4054532fbb742e7c3b4da45c7b3c0bada7cceb0749e112ce190961d580b0115d37369495c38cd87017f51e5fe89ad5d92b6f959b3050572a4d2345256f'
